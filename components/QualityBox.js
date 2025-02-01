31e47063dfcb5cf4ccad5df6261a78261b1b2401
cnzale <azermax123zale@gmail.com>
2025-02-01 06:56:55 +0000

    spaces reduced

--- components/QualityBox.js
+++ components/QualityBox.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const QualityBox = ({ title, description, logo }) => {
   return (
-    <div className='flex flex-col justify-center items-center gap-4 border border-[#1E1E1E] p-8 xl:max-[1515px]:p-6 min-[768px]:max-[1280px]:p-4 rounded-lg w-[600px] xl:max-[1515px]:w-[450px] min-[768px]:max-[1280px]:w-[350px] max-md:w-full max-md:p-3 max-md:gap-2'>
+    <div className='flex flex-col justify-center items-center gap-4 border border-[#1E1E1E] p-8 xl:max-[1515px]:p-6 min-[768px]:max-[1280px]:p-4 rounded-lg w-[600px] xl:max-[1515px]:w-[450px] min-[768px]:max-[1280px]:w-[350px] max-md:w-[350px] max-md:p-3 max-md:gap-2'>
 
       <div className='flex justify-center items-center gap-4 max-md:gap-2'>
         <h1 className='text-3xl font-medium xl:max-[1515px]:text-xl min-[768px]:max-[1280px]:text-lg max-md:text-base'>{title}</h1>
--- components/sections/AboutSection.js
+++ components/sections/AboutSection.js
@@ -3,7 +3,7 @@ import QualityBox from '../QualityBox';
 
 const AboutSection = () => {
   return (
-    <div className='flex flex-col justify-center items-center gap-4 w-full mb-48 ' id='AboutUs'>
+    <div className='flex flex-col justify-center items-center gap-4 w-full mb-48 max-md:mb-28' id='AboutUs'>
       <h1 className='text-4xl font-medium mb-16 xl:max-[1515px]:text-2xl xl:max-[1515px]:mb-10 min-[768px]:max-[1280px]:mb-5 min-[768px]:max-[1280px]:text-2xl max-md:text-center max-md:text-xl max-md:w-full max-md:mb-5 max-md:mt-16  max-md:font-semibold underline'>QUI SOMME NOUS ?</h1>
       <div className='flex justify-center items-center gap-16 w-full max-md:flex-col'>
         {/* Image */}
@@ -30,7 +30,7 @@ const AboutSection = () => {
             alt="Hero"
             width={600}
             height={300}
-            className="object-cover object-top h-[350px] w-[400px] rounded-lg max-md:mb-8"
+            className="object-cover object-top h-[350px] w-[350px] rounded-lg max-md:mb-8"
           />
         </div>
           <QualityBox
@@ -44,7 +44,7 @@ const AboutSection = () => {
             alt="Hero"
             width={600}
             height={300}
-            className="object-cover object-top h-[350px] w-[400px] rounded-lg max-md:mb-8"
+            className="object-cover object-top h-[350px] w-[350px] rounded-lg max-md:mb-8"
           />
         </div>
            <QualityBox
@@ -58,7 +58,7 @@ const AboutSection = () => {
             alt="Hero"
             width={600}
             height={300}
-            className="object-cover object-top h-[350px] w-[400px] rounded-lg max-md:mb-8"
+            className="object-cover object-top h-[350px] w-[350px] rounded-lg max-md:mb-8"
           />
         </div>
         </div>
